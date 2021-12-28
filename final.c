6a54a8ce88440a4ecaa32617ae230f2d7f531f50
Eirene08 <95744397+Eirene08@users.noreply.github.com>
2021-12-28 10:52:22 +0700

    Update final.c

--- final.c
+++ final.c
@@ -10,6 +10,20 @@ int main()
         prnitf("Harap masukkan password\n");
         scanf("%c", pass);
     }
-    
+    int pilih;
+    printf("1. Write a diary\n");
+    printf("2. Read diary entry\n");
+    printf("3. Delete an entry\n");
+    scanf("%d", &pilih);
+    while(pilih != 1 && pilih != 2 && pilih != 3)
+    {
+        printf("Please enter a number based on the choice given\n");
+    }
+    if(pilih == 1)
+    {
+        printf("Write your diary\n");
+        printf("Enter 000 to mark the end of passage\n");
+        
+    }
     return 0;
 }
