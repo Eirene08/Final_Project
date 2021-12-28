472e0fe8c0d3ccafc16137ee70453a362285ed78
Eirene08 <95744397+Eirene08@users.noreply.github.com>
2021-12-28 10:44:26 +0700

    Update final.c

--- final.c
+++ final.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-int main(){
-
-    printf("Test\n");
-    printf("Test 1!\n");
-    printf("Test lagi\n");
+int main()
+{
+    printf("Any password is possible besides false\n");
+    char pass[100];
+    printf("Enter Password:\n");
+    scanf("%c", pass);
+    while (pass == 'false')
+    {
+        prnitf("Harap masukkan password\n");
+        scanf("%c", pass);
+    }
     
     return 0;
 }
